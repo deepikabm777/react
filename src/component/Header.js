e12a97ca987be1d52f2c73666cae34ab05219d13
deepika <manjudarshan149@gmail.com>
2023-11-17 12:29:24 +0530

    first commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -21,11 +21,11 @@ function Header()
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Admission
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">not yet</a></li>
+            <li><a className="dropdown-item" href="#">Another</a></li>
             
             <li><a className="dropdown-item" href="#">Something else here</a></li>
           </ul>
